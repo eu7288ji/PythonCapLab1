426435b9285928d27ef0098b8654d47af3f47e01
eu7288ji <melendez.cj@gmail.com>
2017-08-24 20:22:56 -0500

    added 4th problem and notes

--- camelCase.py
+++ camelCase.py
@@ -1,6 +1,7 @@
-user = raw_input("Type a sentence.")
+user = input("Type a sentence.") #asks for user sentence input
 
 def camelCase(st):
+    #for camelCase, makes sentence title, removes space, lowers first word
     output = ''.join(x for x in st.title() if x.isalpha())
     return output[0].lower() + output[1:]
 
--- numberGame.py
+++ numberGame.py
@@ -1,21 +1,21 @@
-import random
+import random #random generator
 
-guessNumber = 0
+guessNumber = 0 #variable for user guess
 
-number = random.randint(1, 10)
+number = random.randint(1, 10) #thinks of random int between 1 and 10
 print('Guess a number between 1 and 10.')
 
 while guessNumber == 0:
-
+#looping through every time the user guesses between 1 and 10
     guess = int(input('Your Guess: '))
 
-    if guess < number:
+    if guess < number: #if under, too low
         print('Too low...')
 
-    if guess > number:
+    if guess > number: #if over, too high
         print('Too high...')
 
-    if guess == number:
+    if guess == number: #when guessed right, they are congratulated
         print('Cool. ')
         guessNumber = 1
 
